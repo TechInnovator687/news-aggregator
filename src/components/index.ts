01c59770a234e569ebdb0daff684fc1711c35f02
TechInnovator687 <chetanrathore687@gmail.com>
2025-09-22 16:02:23 +0530

    refactor: delete dead components and unused styles

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,2 @@
-export { DropdownSelect } from "./DropdownSelect";
 export { NewsCard } from "./NewsCard";
-export { NewsFeed } from "./NewsFeed";
-export { NewsFilter } from "./NewsFilter";
-export { NewsHeader } from "./NewsHeader";
 export { SelectableChip } from "./SelectableChip";
